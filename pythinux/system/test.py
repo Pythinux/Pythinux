f731a06097417637ae09cebb775711f90265d897
Gordinator <gordinator@gordinator.org>
2024-04-11 14:43:02 +0100

    Updated how libsemver handles semantic versions (breaking change!)
    Package versions used to be formatted in the format a.b.c, but are now
    formatted in the format a.b.c-d, to allow for updates to packages
    without changing the package version. ALL packages will need to update
    their program.ini files to reflect this.

--- pythinux/system/test.py
+++ pythinux/system/test.py
@@ -1,20 +1,11 @@
 import traceback
-help = load_program("help", currentUser, libMode=True)
-shell = load_program("shell", currentUser, libMode=True)
-libargs = load_program("libargs", currentUser, libMode=True)
-var = load_program("var", currentUser, libMode=True)
 
-module = type(__builtins__)
-
-def blank():
-    pass
+libsemver = load_program("libsemver", currentUser, libMode=True)
 
 def main(args):
-    giveOutput("help", currentUser)
-    for program in help.help():
-        try:
-            mod = load_program(program, currentUser, libMode=True)
-            if "main" not in dir(mod):
-                print("ERROR: {} has no main()".format(program))
-        except:
-            pass
+    a, b, c, = "1.0.0", "1.0.0-1", "1.0.0-2"
+    # print(libsemver.check(a))
+    # print(libsemver.check(b))
+    # print(libsemver.check(c))
+    print(libsemver.compare(b,c))
+    print(libsemver.compare(c,b))
--- pythinux/system_low/libsemver.py
+++ pythinux/system_low/libsemver.py
@@ -1,9 +1,9 @@
 import re
 def check(version):
     """
-    Checks if a string is in the format x.y.z, AKA semantic versioning.
+    Checks if a string is in the format x.y.z-a, AKA semantic versioning.
     """
-    pattern = r'^\d+\.\d+\.\d+$'
+    pattern = r'^\d+\.\d+\.\d+-\d+$'
     return bool(re.match(pattern, version))
 def compare(a, b):
     """
@@ -11,15 +11,16 @@ def compare(a, b):
     Returns 1 if a is a higher version than b, -1 if b is a higher version than a, and 0 if both are the same.
     """
     def normalize(version):
+        version = version.replace("-", ".")
         return [int(x) for x in version.split(".")]
 
     if not check(a) or not check(b):
-        raise TypeError("Strings must be in the format x.y.z")
+        raise TypeError("Strings must be in the format x.y.z-a")
 
     version_a = normalize(a)
     version_b = normalize(b)
 
-    for i in range(3):
+    for i in range(4):
         if version_a[i] < version_b[i]:
             return -1
         elif version_a[i] > version_b[i]:
