23ee900430e0b081af137c0eca88d7e1496e0076
Gordinator <gordinator@gordinator.org>
2024-04-25 20:05:03 +0100

    Added `var copy`
    * Added new var.copy() function, which copies variable values.
    
    * Added `var copy`, a frontend for var.copy().
    
    * Created var.CopyError, an exception raised if var.copy() fails.
    
    * All functions in the var module now have documentation.

--- pythinux/system_low/var.py
+++ pythinux/system_low/var.py
@@ -1,4 +1,14 @@
+class CopyError(ValueError):
+    """
+    Exception thrown when `var copy` fails.
+    """
+    def __init__(self, message="var.copy(): Cannot copy invalid variable"):
+        super().__init__(message)
+
 def get(var, fallback=""):
+    """
+    Returns the value of value `var`. If it does not exist, returns `fallback`.
+    """
     vars = giveVars()
     try:
         return giveVars()[var]
@@ -6,15 +16,24 @@ def get(var, fallback=""):
         return fallback
 
 def getint(var, fallback=0):
+    """
+    Returns an integer from a variable. Falls back to `fallback` if the variable is invalid or not an integer.
+    """
     vars = giveVars()
     try:
         return int(giveVars[var])
     except ValueError:
+        # Not an integer
         return fallback
     except KeyError:
+        # Variable does not exist
         return fallback
 
 def getbool(var, fallback=True):
+    """
+    Returns True if the value of `var` is either "true", "True", "yes", "Yes", or "1".
+    Returns False for any other value, and fallback if `var` does not exist.
+    """
     vars = giveVars()
 
     try:
@@ -26,11 +45,17 @@ def getbool(var, fallback=True):
         return fallback
 
 def set(var, val, fallback=""):
+    """
+    Sets variable `var` to `val`.
+    """
     v = giveVars()
     v[var] = val
 
 
 def list():
+    """
+    Prints out a list of variables.
+    """
     var = giveVars()
     div()
     print("Variables")
@@ -42,6 +67,9 @@ def list():
     div()
 
 def rm(name):
+    """
+    Deletes a variable. Returns True if the value was removed.
+    """
     removed = False
     var = giveVars()
     try:
@@ -50,6 +78,16 @@ def rm(name):
     except:
         pass
     return removed
+
+def copy(from_var, to_var):
+    """
+    Copies the value of `from_var` to `to_var`. Raises a CopyError if `from_var` is an invalid variable name.
+    """
+    from_val = get(from_var, fallback=None)
+    if not from_val:
+        raise CopyError
+    set(to_var, from_val)
+
 
 def main(args):
     if args == ["set"]:
@@ -79,6 +117,19 @@ def main(args):
     elif "rm" in args and len(args) == 2:
         if not rm(args[1]):
             print("ERROR: Unable to remove.")
+    elif args == ["copy"]:
+        div()
+        print("var copy <to_var> <from_var>")
+        div()
+        print("Copy a variable onto another variable.")
+        print("E.G: `var copy foo bar` sets `bar` to the value of `foo`.")
+        div()
+    elif "copy" in args and len(args) == 3:
+        args.remove("copy")
+        try:
+            copy(args[0], args[1])
+        except CopyError:
+            print("ERROR: Invalid variable '{}'.".format(args[0]))
     else:
         div()
         print("var [args]")
@@ -86,6 +137,7 @@ def main(args):
         print("Positional Arguments:")
         print("    set: sets a variable")
         print("    get: returns the value of a variable")
+        print("    copy: copy a variable onto another variable")
         print("    rm: delete a variable")
         print("    list: lists all variables")
         div()
