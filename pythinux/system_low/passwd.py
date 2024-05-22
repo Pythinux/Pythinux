d577c48feb409891eb63c5adb615c48400db23e8
Gordinator <gordinator@gordinator.org>
2024-05-22 15:01:24 +0100

    Fixed passwd utility
    The issue was that user.passwd (an invalid attribute) was being set,
    rather than user.password.

--- pythinux/system_low/passwd.py
+++ pythinux/system_low/passwd.py
@@ -1,9 +1,8 @@
 import getpass
 
 def main(args):
-    userList = loadUserList()
     user = userList.byName(currentUser.username)
-    user.passwd = hashString(getpass.getpass("[passwd] Enter your new password $"))
+    user.password = hashString(getpass.getpass("[passwd] Enter your new password $"))
     saveUserList(userList)
     print("Successfully changed password.")
 
