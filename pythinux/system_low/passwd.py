1b19919806cf54a7d3c0761c7fcfc87f41c19862
Gordinator <gordinator@gordinator.org>
2024-05-30 18:32:25 +0100

    The passwd utility now asks for the current password
    This is to prevent abuse from bad actors who take advantage of someone
    staying logged in. Of course, that's a bad idea in general, but at least
    you can't be locked out.
    
    It will also skip this check if the password is blank.

--- pythinux/system_low/passwd.py
+++ pythinux/system_low/passwd.py
@@ -2,7 +2,9 @@ import getpass
 
 def main(args):
     user = userList.byName(currentUser.username)
+    if not verifyHash("", user.password) and not verifyHash(getpass.getpass("[passwd] Enter your current password $"), user.password):
+        print("ERROR: Invalid password.")
+        return
     user.password = hashString(getpass.getpass("[passwd] Enter your new password $"))
     saveUserList(userList)
     print("Successfully changed password.")
-
