b5afc99e98125e75ae83419527bae6522c17daeb
Gordinator <gordinator@gordinator.org>
2024-06-01 13:32:41 +0100

    The `passwd` utility now lets you change other users' passwords
    The passwd utility now performs more advanced system checks before
    deciding whether or not to let you.

--- pythinux/system_low/passwd.py
+++ pythinux/system_low/passwd.py
@@ -1,10 +1,22 @@
 import getpass
 
 def main(args):
-    user = userList.byName(currentUser.username)
-    if not verifyHash("", user.password) and not verifyHash(getpass.getpass("[passwd] Enter your current password $"), user.password):
-        print("ERROR: Invalid password.")
+    if len(args) == 1 and currentUser.group.canSys:
+        user = userList.byName(args[0])
+    elif len(args) == 1:
+        print("ERROR: Access denied.")
         return
+    elif len(args) == 0:
+        user = userList.byName(currentUser.username)
+    else:
+        print("ERROR: Invalid arguments.")
+        return
+
+    if not user.password == "" and not verifyHash("", user.password): 
+        if not verifyHash(getpass.getpass("[passwd] Enter your current password $"), user.password):
+            print("ERROR: Invalid password.")
+            return
+
     user.password = hashString(getpass.getpass("[passwd] Enter your new password $"))
     saveUserList(userList)
     print("Successfully changed password.")
