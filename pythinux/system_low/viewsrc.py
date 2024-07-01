13ea7f1e8d1e7c9ff22a29ab93efb6179c359201
Gordinator <gordinator@gordinator.org>
2024-07-01 19:48:28 +0100

    viewsrc now use file.open()
    This is done in a way that maintains full API compatibility :)
    
    Signed-off-by: Gordinator <gordinator@gordinator.org>

--- pythinux/system_low/viewsrc.py
+++ pythinux/system_low/viewsrc.py
@@ -1,9 +1,14 @@
 which = load_program("which", currentUser, libMode=True)
 
 def view(program):
+    """
+    Returns the source code of a named program as a string.
+    """
     program = which.which([program])[0]
-    with open(program) as f:
+    with file.open(program, currentUser) as f:
         return f.read()
+
+
 def main(args):
     if args:
         for arg in args:
