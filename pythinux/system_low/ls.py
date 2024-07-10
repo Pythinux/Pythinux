a0f8305d4ab40288438a18663fb11dc61677f61e
Gordinator <gordinator@gordinator.org>
2024-07-10 20:06:29 +0100

    Updated ls to 2.0
    * Moved to using libargs instead of manually handling args, which should
      lead to an overall more fluid experience. For now, no *new*
      functionality has been added, but that's soon to change.
    
    Signed-off-by: Gordinator <gordinator@gordinator.org>

--- pythinux/system_low/ls.py
+++ pythinux/system_low/ls.py
@@ -1,11 +1,21 @@
+libargs = load_program("libargs", currentUser, libMode=True)
+
+parser = libargs.ArgumentParser("ls", description="Lists contents of a directory")
+parser.add_argument("directory", help="Directory to list contents of", nargs="?")
+parser.add_argument("--version", "-V", help="Displays version info", action="store_true")
+
+def getFileList(directory, user=None):
+    return os.listdir(file.evalDir(directory, user if user else currentUser))
+
+def formatDirectory(files):
+    div()
+    for file in files:
+        print(file)
+    div()
+
 def main(args):
-    if args == ["--help"]:
-        div()
-        print("ls: lists contents of /")
-        print("ls <directpry>: lists contents of <directory>")
-        div()
-    else:
-        directory = file.evalDir(" ".join(args), currentUser) if args else file.evalDir(CURRDIR, currentUser)
-        div()
-        print("\n".join(os.listdir(directory)))
-        div()
+    args = parser.parse_args(args)
+    if args.version:
+        print("ls 2.0")
+        return
+    formatDirectory(getFileList(args.directory if args.directory else "."))
